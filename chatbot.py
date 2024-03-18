582107b5c44dcb62e11a67bc7d215593e4e1ba84
Mar257Pm <mar95328@outlook.com>
2024-03-18 15:52:14 +0800

    '2+'

--- chatbot.py
+++ chatbot.py
@@ -6,11 +6,15 @@ import logging
 from Firebase import firebase
 
 def main():
+    def start(update, context):
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Hello! I'm your bot.")
     # Load your token and create an Updater for your Bot
     config = configparser.ConfigParser()
     config.read('config.ini')
     updater = Updater(token = (config['TELEGRAM']['ACCESS_TOKEN']), use_context = True)
     dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
     # You can set this logging module, so you will know when 
     # and why things do not work as expected Meanwhile, update your config.ini as:
@@ -37,7 +41,7 @@ def echo(update, context):
     reply_message = update.message.text.upper()
     logging.info("Update: " + str(update))
     logging.info("context: " + str(context))
-    context.bot.send_message(chat_id = update.effective_chat.id, text = reply_message)
+    context.bot.send_message(chat_id = update.effective_chat.id, text = reply_message) 
 
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
